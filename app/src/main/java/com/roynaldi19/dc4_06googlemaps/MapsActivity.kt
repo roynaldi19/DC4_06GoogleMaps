987341290f15fd277860b2866a1566ca640426be
roynaldi19 <roynaldi19@gmail.com>
2023-04-25 18:44:28 +0700

    add lokasi dengan klik lama

--- app/src/main/java/com/roynaldi19/dc4_06googlemaps/MapsActivity.kt
+++ app/src/main/java/com/roynaldi19/dc4_06googlemaps/MapsActivity.kt
@@ -1,13 +1,23 @@
 package com.roynaldi19.dc4_06googlemaps
 
+import android.graphics.Bitmap
+import android.graphics.Canvas
+import android.graphics.Color
 import android.os.Bundle
+import android.util.Log
 import android.view.Menu
 import android.view.MenuItem
+import androidx.annotation.ColorInt
+import androidx.annotation.DrawableRes
 import androidx.appcompat.app.AppCompatActivity
+import androidx.core.content.res.ResourcesCompat
+import androidx.core.graphics.drawable.DrawableCompat
 import com.google.android.gms.maps.CameraUpdateFactory
 import com.google.android.gms.maps.GoogleMap
 import com.google.android.gms.maps.OnMapReadyCallback
 import com.google.android.gms.maps.SupportMapFragment
+import com.google.android.gms.maps.model.BitmapDescriptor
+import com.google.android.gms.maps.model.BitmapDescriptorFactory
 import com.google.android.gms.maps.model.LatLng
 import com.google.android.gms.maps.model.MarkerOptions
 import com.roynaldi19.dc4_06googlemaps.databinding.ActivityMapsBinding
@@ -42,6 +52,14 @@ class MapsActivity : AppCompatActivity(), OnMapReadyCallback {
         )
         map.animateCamera(CameraUpdateFactory.newLatLngZoom(kiddSpace, 15f))
 
+        map.setOnMapLongClickListener { latLng ->
+            map.addMarker(
+                MarkerOptions().position(latLng).title("New Marker")
+                    .snippet("Lat : ${latLng.latitude} long: ${latLng.longitude}")
+                    .icon(vectorToBitmap(R.drawable.ic_android, Color.parseColor("#3DDC84")))
+            )
+        }
+
     }
 
     override fun onCreateOptionsMenu(menu: Menu?): Boolean {
@@ -75,6 +93,26 @@ class MapsActivity : AppCompatActivity(), OnMapReadyCallback {
                 super.onOptionsItemSelected(item)
             }
         }
+    }
+
+    private fun vectorToBitmap(@DrawableRes id: Int, @ColorInt color: Int): BitmapDescriptor {
+        val vectorDrawable = ResourcesCompat.getDrawable(resources, id, null)
+        if (vectorDrawable == null) {
+            Log.e("BitmapHelper", "resource not founf")
+            return BitmapDescriptorFactory.defaultMarker()
+        }
+
+        val bitmap = Bitmap.createBitmap(
+            vectorDrawable.intrinsicWidth,
+            vectorDrawable.intrinsicHeight,
+            Bitmap.Config.ARGB_8888
+        )
+
+        val canvas = Canvas(bitmap)
+        vectorDrawable.setBounds(0, 0, canvas.width, canvas.height)
+        DrawableCompat.setTint(vectorDrawable, color)
+        vectorDrawable.draw(canvas)
+        return BitmapDescriptorFactory.fromBitmap(bitmap)
     }
 
 }
